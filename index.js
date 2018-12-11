a7603cd8cdbda3cf30d7687a42b236228f06655c
Ashok Madhavan Ramesh <AshokMadhavan.Ramesh@siriuscom.com>
2018-12-11 17:37:07 +0530

    hello

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+const port = 5000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 /* GET home page. */
 app.get('/', function(req, res, next) {
-  res.render('index', { title: 'Abhilash' });
+  res.send("I'm awesome");
 });
 
 app.get('/webhook', function(req,res) {
   console.log("From Facebook request ",req);
   console.log("From Facebook ",res);
-  res.render('index',{ title: 'Hey THERE' });
   res.send("HELLO");
 });
